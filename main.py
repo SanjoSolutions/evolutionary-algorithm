1667d116f1db30560f0fc098df7f9d2b8be1a0dd
Jonas Aschenbrenner <jonas.aschenbrenner@interactive-tools.de>
2021-07-03 21:22:19 +0200

    First draft

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
+import itertools
 import random
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import InputLayer, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError
+import numpy as np
+import tensorflow as tf
+
+from road.road import Road
 
 NUMBER_OF_ITERATIONS = 1000
-INITIAL_POPULATION_SIZE = 64
+INITIAL_POPULATION_SIZE = 128
 NUMBER_OF_PARENT_TO_SELECT = int(INITIAL_POPULATION_SIZE / 2)
 
 
+model = Sequential([
+    InputLayer(input_shape=(Road.NUMBER_OF_ROWS * Road.NUMBER_OF_ROADS,)),
+    Dense(32, activation='relu'),
+    Dense(3, activation='softmax')
+])
+
+model.compile(
+    optimizer=Adam(),
+    loss=MeanSquaredError
+)
+
+initial_weights = model.get_weights()
+flatten_weights = [layer_weights.flatten() for layer_weights in initial_weights]
+flatten_weights_2 = [list(layer_weights) for layer_weights in flatten_weights]
+flatten_weights_3 = list(itertools.chain.from_iterable(flatten_weights_2))
+
+weights_length = len(flatten_weights_3)
+
+
 def main():
+    random.seed(0)
     population = generate_initial_population()
-    print(population)
     for iteration in range(NUMBER_OF_ITERATIONS):
         fitness_evaluation = evaluate_fitness(population)
-        parents = select_fittest_individuals(population, fitness_evaluation)
+        sort_population_based_on_fitness(population, fitness_evaluation)
+        print('Scores of top 5:', [fitness_evaluation[individual] for individual in population[:5]])
+        parents = select_fittest_individuals(population)
         children = breed(parents)
-        population = replace_least_fit_individuals(population, children, fitness_evaluation)
-        print(population)
+        population = replace_least_fit_individuals(population, children)
 
 
 def generate_initial_population():
@@ -24,10 +53,10 @@ def generate_initial_population():
 
 
 def generate_individual():
-    return (
-        random.randrange(0, 10 + 1),
-        random.randrange(0, 10 + 1)
-    )
+    weights = [0.0] * weights_length
+    for index in range(weights_length):
+        weights[index] = random.uniform(-1.0, 1.0)
+    return tuple(weights)
 
 
 def evaluate_fitness(population):
@@ -37,13 +66,38 @@ def evaluate_fitness(population):
     return fitness
 
 
+def sort_population_based_on_fitness(population, fitness_evaluation):
+    population.sort(key=lambda individual: fitness_evaluation[individual], reverse=True)
+
+
 def evaluate_fitness_of_individual(individual):
-    return individual[0] * individual[1]
+    weights = [None] * len(initial_weights)
+    a = 0
+    for index in range(len(weights)):
+        initial_weights_2 = initial_weights[index]
+        weights_2 = np.array(individual[a:a + initial_weights_2.size])
+        weights_2 = np.resize(weights_2, initial_weights_2.shape)
+        weights[index] = weights_2
+    model.set_weights(weights)
+
+    random.seed(0)
+    road = Road()
+    done = False
+    total_reward = 0
+    road.reset()
+    while not done:
+        x = np.array([
+            list(itertools.chain.from_iterable(road.get_state()))
+        ])
+        action = tf.math.argmax(model(x)[0]).numpy()
+        state, reward, done = road.step(action)
+        total_reward += reward
+
+    return total_reward
 
 
-def select_fittest_individuals(population, fitness_evaluation):
-    sorted_population = list(sorted(population, key=lambda individual: fitness_evaluation[individual], reverse=True))
-    return sorted_population[:NUMBER_OF_PARENT_TO_SELECT]
+def select_fittest_individuals(population):
+    return population[:NUMBER_OF_PARENT_TO_SELECT]
 
 
 def breed(parents):
@@ -61,9 +115,8 @@ def breed(parents):
     return children
 
 
-def replace_least_fit_individuals(population, children, fitness_evaluation):
-    sorted_population = list(sorted(population, key=lambda individual: fitness_evaluation[individual], reverse=True))
-    return sorted_population[:(len(sorted_population) - len(children))] + children
+def replace_least_fit_individuals(population, children):
+    return population[:(len(population) - len(children))] + children
 
 
 if __name__ == '__main__':
